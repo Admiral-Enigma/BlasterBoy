799f793894ecb227c3ac9eb3a61fbaab1db9738e
Alexander <MrRandomgamerdkHD@gmail.com>
2018-10-15 22:27:26 +0200

    Improvements

--- main.lua
+++ main.lua
@@ -1,10 +1,15 @@
-local lastX = 0
-local lastY = 0
+local CELL_SIZEX = 32
+local CELL_SIZEY = 32
+local ROOM_COUNT = 64
+
+local lastX = 50
+local lastY = 50
 local lastDir = 0
 local hasDrawn = false
+local rooms = {}
 
 function love.load () 
-
+    generateRoom()
 end
 
 function love.update (dt)
@@ -12,39 +17,52 @@ function love.update (dt)
 end
 
 function love.draw ()
-    if hasDrawn == false then
-        -- Generate "random path"
-        for i=1,10 do 
-            local direction = love.math.random(1, 4)
-            
-            while (direction == lastDir) do
-                direction = love.math.random(1, 4)
-            end
+    for k,v in ipairs(rooms) do
+        love.graphics.rectangle("line", v[1], v[2], CELL_SIZEX, CELL_SIZEY)
+    end
+    -- love.graphics.rectangle("fill", x, y, 16, 16)
+end
 
-            if direction == 1 then
-                -- north
-                local x, y = lastX, lastY - 16
-                love.graphics.rectangle("fill", x, y, 16, 16)
-                resetLastPos(x, y, direction)
-            elseif direction == 2 then
-                -- east
-                local x, y = lastX + 16, lastY
-                love.graphics.rectangle("fill", x, y, 16, 16)
-                resetLastPos(x, y, direction)
-            elseif direction == 3 then
-                -- south
-                local x, y = lastX, lastY + 16
-                love.graphics.rectangle("fill", x, y, 16, 16)
-                resetLastPos(x, y, direction)
-            elseif direction == 4 then
-                -- west
-                local x, y = lastX - 16, lastY
-                love.graphics.rectangle("fill", x, y, 16, 16)
-                resetLastPos(x, y, direction)
-            end
+function love.keypressed( key, scancode, isrepeat )
+    if key == "r" then
+        generateRoom()
+    end
+end
+
+function addRoom(x, y)
+    resetLastPos(x, y, direction)
+    table.insert( rooms, {x, y})
+end
+
+function generateRoom()
+    rooms = {}
+    lastX = 50
+    lastY = 50
+    lastDir = 0
+    for i=1,ROOM_COUNT do 
+        local direction = love.math.random(1, 4)
+        
+        if direction == lastDir then
+            direction = love.math.random(1, 4)
         end
-        lastX = 50
-        lastY = 50
+
+        if direction == 1 then
+            -- north
+            local x, y = lastX, lastY - CELL_SIZEY
+            addRoom(x, y)
+        elseif direction == 2 then
+            -- east
+            local x, y = lastX + CELL_SIZEX, lastY
+            addRoom(x, y)
+        elseif direction == 3 then
+            -- south
+            local x, y = lastX, lastY + CELL_SIZEY
+            addRoom(x, y)
+        elseif direction == 4 then
+            -- west
+            local x, y = lastX - CELL_SIZEX, lastY
+            addRoom(x, y)
+        end
     end
 end
 
