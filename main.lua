e39924484a6764b4f7cd8ab794544a13fc05715c
Alexander <MrRandomgamerdkHD@gmail.com>
2018-10-16 14:39:29 +0200

    Added some primitive collider generation

--- main.lua
+++ main.lua
@@ -7,9 +7,11 @@ local lastY = 50
 local lastDir = 0
 local hasDrawn = false
 local rooms = {}
+local colliders = {}
 
 function love.load () 
     generateRoom()
+    generateColliders()
 end
 
 function love.update (dt)
@@ -18,14 +20,47 @@ end
 
 function love.draw ()
     for k,v in ipairs(rooms) do
+        love.graphics.setColor(255, 255, 255, 255)
         love.graphics.rectangle("line", v[1], v[2], CELL_SIZEX, CELL_SIZEY)
     end
-    -- love.graphics.rectangle("fill", x, y, 16, 16)
+    for k,v in ipairs(colliders) do
+        love.graphics.setColor(255, 0, 0, 255)
+        love.graphics.rectangle("fill", v[1], v[2], CELL_SIZEX, CELL_SIZEY)
+    end
 end
 
 function love.keypressed( key, scancode, isrepeat )
     if key == "r" then
         generateRoom()
+        generateColliders()
+    end
+end
+
+function generateColliders()
+    colliders = {}
+    for _,v in ipairs(rooms) do
+        if hasRoom(v[1], v[2] - CELL_SIZEY) == false then
+            -- north
+            local x, y = v[1], v[2] - CELL_SIZEY
+            addCollider(x, y)
+        end
+        if hasRoom(v[1] + CELL_SIZEX, v[2]) == false then
+            -- east
+            local x, y = v[1] + CELL_SIZEX, v[2]
+            addCollider(x, y)
+        end
+        
+        if hasRoom(v[1], v[2] + CELL_SIZEY) == false then
+            -- south
+            local x, y = v[1], v[2] + CELL_SIZEY
+            addCollider(x, y)
+        end
+        
+        if hasRoom(v[1] - CELL_SIZEX, v[2]) == false then
+            -- west
+            local x, y = v[1] - CELL_SIZEX, v[2]
+            addCollider(x, y)
+        end
     end
 end
 
@@ -34,6 +69,10 @@ function addRoom(x, y)
         resetLastPos(x, y, direction)
         table.insert( rooms, {x, y})
     end
+end
+
+function addCollider(x, y)
+    table.insert(colliders, {x, y})
 end
 
 function hasRoom(x, y)
