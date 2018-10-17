cb68cf17f8b1f89c0051f393221ce2b5b1e63356
Alexander <MrRandomgamerdkHD@gmail.com>
2018-10-17 19:44:38 +0200

    Moved the map generator into a separate class

---
+++ MapGenerator.lua
@@ -0,0 +1,82 @@
+CELL_SIZEX = 32
+CELL_SIZEY = 32
+local ROOM_COUNT = 64
+local START_X = 50
+local START_Y = 50
+
+local MapGenerator = {}
+MapGenerator.__index = MapGenerator
+
+function MapGenerator:new()              
+    local ins = setmetatable({}, self)
+    ins.rooms = {}
+    ins.colliders = {}
+    ins.lastX = START_X
+    ins.lastY = START_Y
+    ins.lastDir = 0
+    return ins
+end
+
+function MapGenerator:generateRooms()
+    self:clearVars()
+    
+    for i=1,ROOM_COUNT do
+        local direction = love.math.random(1, 4)
+        
+        if direction == self.lastDir then
+            direction = love.math.random(1, 4)
+        end
+
+        if direction == 1 then
+            -- north
+            local x, y = self.lastX, self.lastY - CELL_SIZEY
+            self:addRoom(x, y)
+        elseif direction == 2 then
+            -- east
+            local x, y = self.lastX + CELL_SIZEX, self.lastY
+            self:addRoom(x, y)
+        elseif direction == 3 then
+            -- south
+            local x, y = self.lastX, self.lastY + CELL_SIZEY
+            self:addRoom(x, y)
+        elseif direction == 4 then
+            -- west
+            local x, y = self.lastX - CELL_SIZEX, self.lastY
+            self:addRoom(x, y)
+        end
+    end
+end
+
+function MapGenerator:addRoom(x, y)
+    if self:hasRoom(x, y) == false then
+        self:resetLastPos(x, y, direction)
+        table.insert(self.rooms,{x = x, y = y})
+    end 
+end
+
+function MapGenerator:hasRoom(x, y)
+    local has = false
+    for _,v in ipairs(self.rooms) do
+        if v.x == x then
+            if v.y == y then
+                has = true
+            end
+        end
+    end
+    return has
+end
+
+function MapGenerator:clearVars()
+    self.rooms = {}
+    self.lastX = START_X
+    self.lastY = START_Y
+    self.lastDir = 0
+end
+
+function MapGenerator:resetLastPos(x, y, dir)
+    self.lastX = x
+    self.lastY = y
+    self.lastDir = dir
+end
+
+return MapGenerator
--- main.lua
+++ main.lua
@@ -1,16 +1,8 @@
-local CELL_SIZEX = 32
-local CELL_SIZEY = 32
-local ROOM_COUNT = 64
-
-local lastX = 50
-local lastY = 50
-local lastDir = 0
-local rooms = {}
-local colliders = {}
+local MapGenerator = require "MapGenerator"
 
 function love.load () 
-    generateRoom()
-    generateColliders()
+    map = MapGenerator:new()
+    map:generateRooms()
 end
 
 function love.update (dt)
@@ -18,108 +10,15 @@ function love.update (dt)
 end
 
 function love.draw ()
-    for k,v in ipairs(rooms) do
+    for k,v in ipairs(map.rooms) do
         love.graphics.setColor(255, 255, 255, 255)
-        love.graphics.rectangle("line", v[1], v[2], CELL_SIZEX, CELL_SIZEY)
+        love.graphics.rectangle("line", v.x, v.y, CELL_SIZEX, CELL_SIZEY)
     end
-    for k,v in ipairs(colliders) do
-        love.graphics.setColor(255, 0, 0, 255)
-        love.graphics.rectangle("fill", v[1], v[2], CELL_SIZEX, CELL_SIZEY)
-    end
+
 end
 
 function love.keypressed( key, scancode, isrepeat )
     if key == "r" then
-        generateRoom()
-        generateColliders()
+        map:generateRooms()
     end
-end
-
-function generateColliders()
-    colliders = {}
-    for _,v in ipairs(rooms) do
-        if hasRoom(v[1], v[2] - CELL_SIZEY) == false then
-            -- north
-            local x, y = v[1], v[2] - CELL_SIZEY
-            addCollider(x, y)
-        end
-        if hasRoom(v[1] + CELL_SIZEX, v[2]) == false then
-            -- east
-            local x, y = v[1] + CELL_SIZEX, v[2]
-            addCollider(x, y)
-        end
-        
-        if hasRoom(v[1], v[2] + CELL_SIZEY) == false then
-            -- south
-            local x, y = v[1], v[2] + CELL_SIZEY
-            addCollider(x, y)
-        end
-        
-        if hasRoom(v[1] - CELL_SIZEX, v[2]) == false then
-            -- west
-            local x, y = v[1] - CELL_SIZEX, v[2]
-            addCollider(x, y)
-        end
-    end
-end
-
-function addRoom(x, y)
-    if hasRoom(x, y) == false then
-        resetLastPos(x, y, direction)
-        table.insert( rooms, {x, y})
-    end
-end
-
-function addCollider(x, y)
-    table.insert(colliders, {x, y})
-end
-
-function hasRoom(x, y)
-    local has = false
-    for _,v in ipairs(rooms) do
-        if v[1] == x then
-            if v[2] == y then
-                has = true
-            end
-        end
-    end
-    return has
-end
-
-function generateRoom()
-    rooms = {}
-    lastX = 50
-    lastY = 50
-    lastDir = 0
-    for i=1,ROOM_COUNT do 
-        local direction = love.math.random(1, 4)
-        
-        if direction == lastDir then
-            direction = love.math.random(1, 4)
-        end
-
-        if direction == 1 then
-            -- north
-            local x, y = lastX, lastY - CELL_SIZEY
-            addRoom(x, y)
-        elseif direction == 2 then
-            -- east
-            local x, y = lastX + CELL_SIZEX, lastY
-            addRoom(x, y)
-        elseif direction == 3 then
-            -- south
-            local x, y = lastX, lastY + CELL_SIZEY
-            addRoom(x, y)
-        elseif direction == 4 then
-            -- west
-            local x, y = lastX - CELL_SIZEX, lastY
-            addRoom(x, y)
-        end
-    end
-end
-
-function resetLastPos(x, y, dir)
-    lastX = x
-    lastY = y
-    lastDir = dir
 end
