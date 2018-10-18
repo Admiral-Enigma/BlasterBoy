c09c2813881fc9375c8318f222e98f484583a5d7
Alexander <MrRandomgamerdkHD@gmail.com>
2018-10-18 19:13:03 +0200

    Added a testing camera

--- main.lua
+++ main.lua
@@ -1,8 +1,19 @@
+-- Dependencies
+local Camera = require "lib.hump.camera"
+
+
+-- Map
 local MapRenderer = require "MapRenderer"
 local map
 
+local cam
+-- TODO: Make some sort of global file
+local CAM_ZOOM = 10
+
 function love.load () 
     map = MapRenderer:new()
+    cam = Camera(0,0)
+    cam:zoomTo(CAM_ZOOM)
 end
 
 function love.update (dt)
@@ -10,12 +21,20 @@ function love.update (dt)
 end
 
 function love.draw ()
+    cam:attach()
     map:draw()
-
+    cam:detach()
 end
 
 function love.keypressed( key, scancode, isrepeat )
     if key == "r" then
         map:generateMap()
+    end
+    if key == "p" then
+        if cam.scale == CAM_ZOOM then
+            cam:zoomTo(1)
+        else
+            cam:zoomTo(CAM_ZOOM)
+        end
     end
 end
