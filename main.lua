a6297ef5e4f8d4d2efa3dd641a0bd82ae1782713
Alexander <MrRandomgamerdkHD@gmail.com>
2018-10-18 19:49:45 +0200

    Added a test player for testing out the camera

--- main.lua
+++ main.lua
@@ -1,28 +1,62 @@
 -- Dependencies
-local Camera = require "lib.hump.camera"
+local Camera = require 'lib.hump.camera'
 
 
--- Map
-local MapRenderer = require "MapRenderer"
+-- Objects
+local MapRenderer = require 'MapRenderer'
 local map
-
 local cam
--- TODO: Make some sort of global file
-local CAM_ZOOM = 10
+-- TEST
+local player = {}
+player.speed = 200
+
+-- Constants
+local CAM_ZOOM = 1
+
+
+local drawCol = true
 
 function love.load () 
+    love.graphics.setDefaultFilter('nearest', 'nearest', 0)
     map = MapRenderer:new()
     cam = Camera(0,0)
-    cam:zoomTo(CAM_ZOOM)
+
+    player.sprite = love.graphics.newImage('assets/happyboi.png')
+    player.x = 64
+    player.y = 64
+
+
 end
 
 function love.update (dt)
+    local scale = 4
+    if love.keyboard.isDown('w') then
+        player.y = player.y - player.speed * dt
+    elseif love.keyboard.isDown('s') then
+        player.y = player.y + player.speed * dt
+    end
 
+    if love.keyboard.isDown('d') then
+        player.x = player.x + player.speed * dt
+    elseif love.keyboard.isDown('a') then
+        player.x = player.x - player.speed * dt
+    end
+    cam:lockX(player.x * scale + 8) -- times scale + half of player width
+    cam:lockY(player.y * scale + 8)
 end
 
 function love.draw ()
     cam:attach()
-    map:draw()
+    love.graphics.scale(4, 4)
+    love.graphics.setColor(255, 255, 255, 255)
+    love.graphics.circle("fill", 0, 0, 5)
+    if drawCol then
+        map:drawColliders()
+    end
+    map:drawMap()
+
+    love.graphics.draw(player.sprite, player.x, player.y)
+
     cam:detach()
 end
 
@@ -31,10 +65,6 @@ function love.keypressed( key, scancode, isrepeat )
         map:generateMap()
     end
     if key == "p" then
-        if cam.scale == CAM_ZOOM then
-            cam:zoomTo(1)
-        else
-            cam:zoomTo(CAM_ZOOM)
-        end
+        drawCol = not drawCol
     end
 end
