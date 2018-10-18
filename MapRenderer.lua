52fe621bbcee6da3350d951d5c705b0c68fc3d4b
Alexander <MrRandomgamerdkHD@gmail.com>
2018-10-18 19:49:23 +0200

    Seperated drawing functions

--- MapRenderer.lua
+++ MapRenderer.lua
@@ -11,16 +11,20 @@ function MapRenderer:new()
     return ins
 end
 
-function MapRenderer:draw()
+function MapRenderer:drawMap()
     for k,v in ipairs(self.mapGen.rooms) do
         love.graphics.setColor(255, 255, 255, 255)
         love.graphics.rectangle("line", v.x, v.y, CELL_SIZEX, CELL_SIZEY)
     end
+
+end
+function MapRenderer:drawColliders()
     for k,v in ipairs(self.mapGen.colliders) do
         love.graphics.setColor(255, 0, 0, 255)
         love.graphics.rectangle("fill", v.x, v.y, CELL_SIZEX, CELL_SIZEY)
     end
 end
+
 
 function MapRenderer:generateMap()
     self.mapGen:generateRooms()
