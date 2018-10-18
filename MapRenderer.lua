bb7841f23f8c56eb47b44a93cca21f2957593614
Alexander <MrRandomgamerdkHD@gmail.com>
2018-10-18 18:21:57 +0200

    Moved map rendering code into a class

---
+++ MapRenderer.lua
@@ -0,0 +1,30 @@
+local MapGenerator = require "MapGenerator"
+
+local MapRenderer = {}
+MapRenderer.__index = MapRenderer
+
+function MapRenderer:new()
+    local ins = setmetatable({}, self)
+    ins.mapGen = MapGenerator:new()
+    ins.mapGen:generateRooms()
+    ins.mapGen:generateColliders()
+    return ins
+end
+
+function MapRenderer:draw()
+    for k,v in ipairs(self.mapGen.rooms) do
+        love.graphics.setColor(255, 255, 255, 255)
+        love.graphics.rectangle("line", v.x, v.y, CELL_SIZEX, CELL_SIZEY)
+    end
+    for k,v in ipairs(self.mapGen.colliders) do
+        love.graphics.setColor(255, 0, 0, 255)
+        love.graphics.rectangle("fill", v.x, v.y, CELL_SIZEX, CELL_SIZEY)
+    end
+end
+
+function MapRenderer:generateMap()
+    self.mapGen:generateRooms()
+    self.mapGen:generateColliders()
+end
+
+return MapRenderer
--- main.lua
+++ main.lua
@@ -1,9 +1,8 @@
-local MapGenerator = require "MapGenerator"
+local MapRenderer = require "MapRenderer"
+local map
 
 function love.load () 
-    map = MapGenerator:new()
-    map:generateRooms()
-    map:generateColliders()
+    map = MapRenderer:new()
 end
 
 function love.update (dt)
@@ -11,20 +10,12 @@ function love.update (dt)
 end
 
 function love.draw ()
-    for k,v in ipairs(map.rooms) do
-        love.graphics.setColor(255, 255, 255, 255)
-        love.graphics.rectangle("line", v.x, v.y, CELL_SIZEX, CELL_SIZEY)
-    end
-    for k,v in ipairs(map.colliders) do
-        love.graphics.setColor(255, 0, 0, 255)
-        love.graphics.rectangle("fill", v.x, v.y, CELL_SIZEX, CELL_SIZEY)
-    end
+    map:draw()
 
 end
 
 function love.keypressed( key, scancode, isrepeat )
     if key == "r" then
-        map:generateRooms()
-        map:generateColliders()
+        map:generateMap()
     end
 end
