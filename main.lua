fbb34344c5ae20440eaffe1d6bb5b6e2bca3897e
Alexander <MrRandomgamerdkHD@gmail.com>
2018-10-19 19:11:03 +0200

    Moved player into seperate class

--- main.lua
+++ main.lua
@@ -1,14 +1,14 @@
 -- Dependencies
 local Camera = require 'lib.hump.camera'
 Globals = require 'Globals'
+local Player = require 'objects.Player'
 
 -- Objects
 local MapRenderer = require 'MapRenderer'
 local map
 local cam
 -- TEST
-local player = {}
-player.speed = 200
+local player
 
 -- Constants
 local CAM_ZOOM = 1
@@ -21,25 +21,14 @@ function love.load ()
     map = MapRenderer:new()
     cam = Camera(0,0)
 
-    player.sprite = love.graphics.newImage('assets/happyboi.png')
-    player.x = 64
-    player.y = 64
+    player = Player:new(64, 64)
+
 
 
 end
 
 function love.update (dt)
-    if love.keyboard.isDown('w') then
-        player.y = player.y - player.speed * dt
-    elseif love.keyboard.isDown('s') then
-        player.y = player.y + player.speed * dt
-    end
-
-    if love.keyboard.isDown('d') then
-        player.x = player.x + player.speed * dt
-    elseif love.keyboard.isDown('a') then
-        player.x = player.x - player.speed * dt
-    end
+    player:update(dt)
     cam:lockX(player.x * Globals.scale + 8) -- times scale + half of player width
     cam:lockY(player.y * Globals.scale + 8)
 end
@@ -56,8 +45,7 @@ function love.draw ()
     love.graphics.setColor(255,0,144, 200)
     love.graphics.circle("fill", 0, 0, 5)
 
-    love.graphics.setColor(255,255,255, 255)
-    love.graphics.draw(player.sprite, player.x, player.y)
+    player:draw()
 
     cam:detach()
 end
---
+++ objects/Player.lua
@@ -0,0 +1,33 @@
+local WALKSPEED = 200
+
+local Player = {}
+Player.__index = Player
+
+function Player:new(x, y)
+    local ins = setmetatable({}, self)
+    ins.sprite = love.graphics.newImage('assets/happyboi.png')
+    ins.x = x
+    ins.y = y
+    return ins
+end
+
+function Player:draw()
+    love.graphics.setColor(255,255,255, 255)
+    love.graphics.draw(self.sprite, self.x, self.y)
+end
+
+function Player:update(dt)
+    if love.keyboard.isDown('w') then
+        self.y = self.y - WALKSPEED * dt
+    elseif love.keyboard.isDown('s') then
+        self.y = self.y + WALKSPEED * dt
+    end
+
+    if love.keyboard.isDown('d') then
+        self.x = self.x + WALKSPEED * dt
+    elseif love.keyboard.isDown('a') then
+        self.x = self.x - WALKSPEED * dt
+    end
+end
+
+return Player
