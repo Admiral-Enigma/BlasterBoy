05475ad44839d88f7610e64c0f47e45468be1fd1
Alexander <MrRandomgamerdkHD@gmail.com>
2018-10-20 21:03:20 +0200

    Nice "Game feel" ;D

--- objects/Player.lua
+++ objects/Player.lua
@@ -1,4 +1,6 @@
-local WALKSPEED = 200
+local WALKSPEED = 800
+local FRICTION = 9
+local MAX_SPEED = 900
 
 local Player = {}
 Player.__index = Player
@@ -8,6 +10,8 @@ function Player:new(x, y)
     ins.sprite = love.graphics.newImage('assets/happyboi.png')
     ins.x = x
     ins.y = y
+    ins.vx = 0
+    ins.vy = 0
     ins.width = 16
     ins.height = 16
     return ins
@@ -24,7 +28,7 @@ function Player:setPosition(x, y)
 end
 
 function Player:update(dt)
-    
+    --[[
     local dx, dy = 0, 0
     if love.keyboard.isDown('w') then
         dy = -WALKSPEED * dt
@@ -36,11 +40,27 @@ function Player:update(dt)
         dx = WALKSPEED * dt
     elseif love.keyboard.isDown('a') then
         dx = -WALKSPEED * dt
-    end
+    end]]
 
     -- We moving
-    if dx ~= 0 or dy ~= 0 then
-        self.x, self.y, _, _ = world:move(self, self.x + dx, self.y + dy)
+    if self.vx ~= 0 or self.vy ~= 0 then
+        self.x, self.y, _, _ = world:move(self, self.x + self.vx * dt, self.y + self.vy * dt)
+    end
+
+    self.vx = self.vx * (1 - math.min(dt * FRICTION, 1))
+    self.vy = self.vy * (1 - math.min(dt * FRICTION, 1))
+
+    -- Input
+    if love.keyboard.isDown('w') and self.vy > -MAX_SPEED then
+        self.vy = self.vy - WALKSPEED * dt
+    elseif love.keyboard.isDown('s') and self.vy < MAX_SPEED then
+        self.vy = self.vy + WALKSPEED * dt
+    end
+
+    if love.keyboard.isDown('d') and self.vx < MAX_SPEED then
+        self.vx = self.vx + WALKSPEED * dt
+    elseif love.keyboard.isDown('a') and self.vx > -MAX_SPEED then
+        self.vx = self.vx - WALKSPEED * dt
     end
 end
 
