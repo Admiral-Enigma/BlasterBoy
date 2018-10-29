42defbe9530e4fe55e9a0d26e238602d69d68770
Alexander <MrRandomgamerdkHD@gmail.com>
2018-10-29 16:49:39 +0100

    More debug info

--- utils.lua
+++ utils.lua
@@ -1,5 +1,12 @@
 function drawDebug()
     love.graphics.setColor(255,255,255, 255)
-    love.graphics.print("Current FPS: "..tostring(love.timer.getFPS( )), 10, 10)
-    love.graphics.print("Entities: "..tostring(Globals.EntityFactory.count), 10, 20)
+    love.graphics.print("Current FPS: "..tostring(love.timer.getFPS( )), 10, 15)
+    love.graphics.print("Entities: "..tostring(Globals.EntityFactory.count), 10, 15 * 2)
+    local stats = love.graphics.getStats()
+    love.graphics.print("DrawCalls: "..tostring(stats.drawcalls), 10, 15 * 3)
+
+    local str = string.format("Estimated amount of texture memory used: %.2f KB", stats.texturememory / 1024)
+    love.graphics.print(str, 10, 15 * 4)
+    love.graphics.print("Images loaded: "..tostring(stats.images), 10, 15 * 5)
+
 end
